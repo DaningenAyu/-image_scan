d12c2ec80d26e9fd8b03f28a27ae9f4b16c6f3d4
Ayu <108796833+DaningenAyu@users.noreply.github.com>
2023-04-17 18:04:27 +0900

    update 2023/04/17.18:04

--- view_color.py
+++ view_color.py
@@ -14,24 +14,27 @@ class ViewColor():
     def __init__(self):
         self.bridge = CvBridge()
         rospy.Subscriber('/camera/color/image_raw',Image,self.listener)
+        self.flg = 0
 
     def listener(self,data):
         try:
             self.bridge_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
-            cv2.imshow('viewHSV',self.bridge_image)
-            cv2.waitKey(1)
+            if self.flg == 0:
+                cv2.imshow('viewHSV',self.bridge_image)
+                cv2.waitKey(1)
             cv2.setMouseCallback('viewHSV',self.call_mouse_pos)
         except CvBridgeError as e:
             print(e)
 
     def call_mouse_pos(self,event,x,y,flags,params):
         img = np.copy(self.bridge_image)
-        if event == cv2.EVENT_MOUSEMOVE:
+        if event == cv2.EVENT_LBUTTONDBLCLK:
             hsvclor = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
             cv2.circle(img,center=(x,y),radius=5,color=(0,0,0),thickness=-1)
             h,s,v = hsvclor[y,x,:]
             hsv_str = '(H,S,V)=(' + str(h) + ',' + str(s) +','+ str(v) + ')'
-            cv2.putText(img,hsv_str,(30,50),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2,cv2.LINE_AA)
+            #cv2.putText(img,hsv_str,(30,50),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2,cv2.LINE_AA)
+            print(hsv_str)
         cv2.imshow('viewHSV',img)
 
 
