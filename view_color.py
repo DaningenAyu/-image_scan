d0e670ca37058f41c13735dcbfc145f49f9bb38e
Ayu <108796833+DaningenAyu@users.noreply.github.com>
2023-04-17 17:25:47 +0900

    update_view_2023/04/17.17:25

--- view_color.py
+++ view_color.py
@@ -4,39 +4,42 @@
 import cv2
 import numpy as np
 import rospy
+import sys
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge,CvBridgeError
 
 
-class View_realsense_color():
+class ViewColor():
+
     def __init__(self):
-        rospy.init_node("view_hsv_color",anonymous=True)
         self.bridge = CvBridge()
         rospy.Subscriber('/camera/color/image_raw',Image,self.listener)
-        cv2.namedWindow("viewHSV",cv2.WINDOW_NORMAL)
-        cv2.setMouseCallback("viewHSV",self.call_mouse_pos) 
-    
+
     def listener(self,data):
         try:
-            self.cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
-            cv2.imshow("viewHSV",self.cv_image)
+            self.bridge_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+            cv2.imshow('viewHSV',self.bridge_image)
+            cv2.waitKey(1)
+            cv2.setMouseCallback('viewHSV',self.call_mouse_pos)
         except CvBridgeError as e:
             print(e)
 
     def call_mouse_pos(self,event,x,y,flags,params):
-        print("eee")
+        img = np.copy(self.bridge_image)
         if event == cv2.EVENT_MOUSEMOVE:
-            self.img = np.copy(self.cv_image)
-            self.img = cv2.cvtColor(self.img,cv2.COLOR_BGR2HSV_FULL)
-            H,S,V = self.img[y,x,:]
-            hsv_str = '(H,S,V)=(' + str(H) + ',' + str(S) +','+ str(V) + ')'
-            cv2.putText(self.img,hsv_str,(30,50),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2,cv2.LINE_AA)
-            print("fff")
-            cv2.imshow('viewHSV',self.img)
+            hsvclor = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+            cv2.circle(img,center=(x,y),radius=5,color=(0,0,0),thickness=-1)
+            h,s,v = hsvclor[y,x,:]
+            hsv_str = '(H,S,V)=(' + str(h) + ',' + str(s) +','+ str(v) + ')'
+            cv2.putText(img,hsv_str,(30,50),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),2,cv2.LINE_AA)
+        cv2.imshow('viewHSV',img)
+
 
 if __name__ == '__main__':
     try:
-        view = View_realsense_color()
+        rospy.init_node("view_hsv_color",anonymous=True)
+        ViewColor()
         rospy.spin()
-    except rospy.ROSInterruptException as e:
-        print(e)
+    except KeyboardInterrupt:
+        print("Shutting down")
+        cv2.destroyWindow('viewHSV')
